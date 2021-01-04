a6a7528035fb8b8b93542590e4d696d89def996f
Dalinger <dalinger@skbkontur.ru>
2021-01-04 22:36:37 +0500

    Определение настроек текущего старта вынесена в отделный метод с результатом-классом

--- src/Program.cs
+++ src/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
@@ -25,102 +26,39 @@ namespace webdiff
 			CestLaVie = 2
 		}
 
+        private class Configuration
+        {
+            public Configuration()
+            {
+                Output = ".";
+                Profile = "profile.toml";
+                Template = "template.html";
+                ShowHelpAndExit = false;
+                Error = Error.None;
+			}
+
+            public string Output;
+            public string Profile;
+            public string Template;
+            public bool ShowHelpAndExit;
+            public Error Error;
+            public Settings Settings;
+            public Uri SvcLeft;
+            public Uri SvcRight;
+            public string Input;
+        }
+
 		private static int Main(string[] args)
-		{
-			Error error = 0;
+        {
+            var cfg = ProcessConfiguration(args);
+			if (cfg.Error != Error.None)
+                return (int)cfg.Error;
 
-			var output = ".";
-			var profile = "profile.toml";
-			var template = "template.html";
-			var showHelpAndExit = false;
-
-			var options = new OptionSet
-			{
-				{"o|output=", $"Reports output directory\n(default: '{output}')", v => output = v},
-				{"p|profile=", $"Profile TOML file with current settings\n(default: '{profile}')", v => profile = v},
-				{"t|template=", $"HTML report template file\n(default: '{template}')", v => template = v},
-				{"h|help", "Show this message", v => showHelpAndExit = v != null}
-			};
-
-			List<string> free = null;
+            Cookie[] cookies = null;
 			try
 			{
-				free = options.Parse(args);
-			}
-			catch(OptionException e)
-			{
-				Console.Error.WriteLine("Option '{0}' value is invalid: {1}", e.OptionName, e.Message);
-				Console.Error.WriteLine();
-				error = Error.InvalidArgs;
-			}
-
-			if(!PathHelper.FileExistsWithOptionalExt(ref profile, ".toml"))
-			{
-				Console.Error.WriteLine($"Profile '{profile}' not found");
-				Console.Error.WriteLine();
-				error = Error.InvalidArgs;
-			}
-
-			if(!PathHelper.FileExistsWithOptionalExt(ref template, ".html"))
-			{
-				Console.Error.WriteLine($"Template '{template}' not found");
-				Console.Error.WriteLine();
-				error = Error.InvalidArgs;
-			}
-
-			Settings settings = null;
-			try
-			{
-				settings = Settings.Read(profile.RelativeToBaseDirectory());
-			}
-			catch(Exception e)
-			{
-				Console.Error.WriteLine($"Failed to read profile '{profile}': {e.Message}");
-				Console.Error.WriteLine();
-				error = Error.InvalidArgs;
-			}
-
-			try
-			{
-				if(free?.Count >= 2)
-				{
-					SvcLeft = new Uri(free[0]);
-					SvcRight = new Uri(free[1]);
-				}
-			}
-			catch(Exception e)
-			{
-				Console.Error.WriteLine(e.Message);
-				Console.Error.WriteLine();
-				error = Error.InvalidArgs;
-			}
-
-			var input = free?.Skip(2).FirstOrDefault();
-			if(input != null && !File.Exists(input.RelativeToBaseDirectory()))
-			{
-				Console.Error.WriteLine("Input file with URLs not found");
-				Console.Error.WriteLine();
-				error = Error.InvalidArgs;
-			}
-
-			if(showHelpAndExit || error != 0 || settings == null || free == null || free.Count < 2)
-			{
-				Console.WriteLine("Usage: webdiff [OPTIONS] URL1 URL2 [FILE]");
-				Console.WriteLine("Options:");
-				options.WriteOptionDescriptions(Console.Out);
-				Console.WriteLine();
-				Console.WriteLine("Examples:");
-				Console.WriteLine("  webdiff http://prod.example.com http://test.example.com < URLs.txt");
-				Console.WriteLine("  webdiff -p profile.toml -t template.html -o data http://prod.example.com http://test.example.com URLs.txt");
-				Console.WriteLine();
-				return (int)error;
-			}
-
-			Cookie[] cookies = null;
-			try
-			{
-				if(settings.Driver.Cookies != null)
-					cookies = CookieParser.Parse(settings.Driver.Cookies.RelativeToBaseDirectory());
+				if(cfg.Settings.Driver.Cookies != null)
+					cookies = CookieParser.Parse(cfg.Settings.Driver.Cookies.RelativeToBaseDirectory());
 			}
 			catch(Exception e)
 			{
@@ -132,7 +70,7 @@ namespace webdiff
 
 			try
 			{
-				ResultsPath = Path.Combine(output, started.ToString("yyyyMMdd-HHmmss")).RelativeToBaseDirectory();
+				ResultsPath = Path.Combine(cfg.Output, started.ToString("yyyyMMdd-HHmmss")).RelativeToBaseDirectory();
 				ResultsImgPath = Path.Combine(ResultsPath, ImgRelativePath);
 
 				Directory.CreateDirectory(ResultsPath);
@@ -148,14 +86,14 @@ namespace webdiff
 
 			try
 			{
-				drivers = new[] {SvcLeft, SvcRight}
+				drivers = new[] {cfg.SvcLeft, cfg.SvcRight}
 					.AsParallel().AsOrdered()
 					.Select(uri =>
 					{
 						RemoteWebDriver driver = null;
 						try
 						{
-							driver = Startup.StartNewDriver(settings.Driver, settings.Mobile).SetWindowSettings(settings.Window);
+							driver = Startup.StartNewDriver(cfg.Settings.Driver, cfg.Settings.Mobile).SetWindowSettings(cfg.Settings.Window);
 							Console.Error.WriteLine("Started driver, {0}", driver.Capabilities);
 							driver.Url = uri.ToString();
 						}
@@ -175,24 +113,24 @@ namespace webdiff
 				var results = new Results
 				{
 					Started = started,
-					LeftBase = SvcLeft,
-					RightBase = SvcRight,
-					Profile = profile,
+					LeftBase = cfg.SvcLeft,
+					RightBase = cfg.SvcRight,
+					Profile = cfg.Profile,
 					Diffs = new List<Diff>()
 				};
 
 				int errors =
-					(input == null ? Console.In.ReadLines() : File.ReadLines(input.RelativeToBaseDirectory()))
+					(cfg.Input == null ? Console.In.ReadLines() : File.ReadLines(cfg.Input.RelativeToBaseDirectory()))
 					.Select(line => line.Trim())
 					.Where(line => line != string.Empty)
 					.Where(line => !line.StartsWith("#", StringComparison.Ordinal))
-					.Select((line, idx) => Cmp(settings, drivers, line, idx, results))
+					.Select((line, idx) => Cmp(cfg, drivers, line, idx, results))
 					.Count(res => !res);
 
 				results.Ended = DateTime.Now;
 				results.Elapsed = results.Ended - results.Started;
 
-				File.Copy(template.RelativeToBaseDirectory(), Path.Combine(ResultsPath, HtmlReportFilename));
+				File.Copy(cfg.Template.RelativeToBaseDirectory(), Path.Combine(ResultsPath, HtmlReportFilename));
 				File.WriteAllText(Path.Combine(ResultsPath, JsResultsFilename), $"{JsRenderFunctionName}({JsonConvert.SerializeObject(results, Formatting.Indented, new SizeConverter(), new RectangleConverter())});{Environment.NewLine}");
 
 				Console.WriteLine();
@@ -213,9 +151,99 @@ namespace webdiff
 			}
 		}
 
-		//TODO: Errors handling and refactoring
-		private static bool Cmp(Settings settings, (Uri BaseUri, RemoteWebDriver Driver)[] items, string relative, int idx, Results results)
-		{
+        private static Configuration ProcessConfiguration(string[] args)
+        {
+            Configuration cfg = new();
+
+            var options = new OptionSet
+            {
+                {"o|output=", $"Reports output directory\n(default: '{cfg.Output}')", v => cfg.Output = v},
+                {"p|profile=", $"Profile TOML file with current settings\n(default: '{cfg.Profile}')", v => cfg.Profile = v},
+                {"t|template=", $"HTML report template file\n(default: '{cfg.Template}')", v => cfg.Template = v},
+                {"h|help", "Show this message", v => cfg.ShowHelpAndExit = v != null}
+            };
+
+            List<string> free = null;
+            try
+            {
+                free = options.Parse(args);
+            }
+            catch (OptionException e)
+            {
+                Console.Error.WriteLine("Option '{0}' value is invalid: {1}", e.OptionName, e.Message);
+                Console.Error.WriteLine();
+                cfg.Error = Error.InvalidArgs;
+            }
+
+            if (!PathHelper.FileExistsWithOptionalExt(ref cfg.Profile, ".toml"))
+            {
+                Console.Error.WriteLine($"Profile '{cfg.Profile}' not found");
+                Console.Error.WriteLine();
+                cfg.Error = Error.InvalidArgs;
+            }
+
+            if (!PathHelper.FileExistsWithOptionalExt(ref cfg.Template, ".html"))
+            {
+                Console.Error.WriteLine($"Template '{cfg.Template}' not found");
+                Console.Error.WriteLine();
+                cfg.Error = Error.InvalidArgs;
+            }
+
+            try
+            {
+                cfg.Settings = Settings.Read(cfg.Profile.RelativeToBaseDirectory());
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine($"Failed to read profile '{cfg.Profile}': {e.Message}");
+                Console.Error.WriteLine();
+                cfg.Error = Error.InvalidArgs;
+            }
+
+            try
+            {
+                if (free?.Count >= 2)
+                {
+                    cfg.SvcLeft = new Uri(free[0]);
+                    cfg.SvcRight = new Uri(free[1]);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine(e.Message);
+                Console.Error.WriteLine();
+                cfg.Error = Error.InvalidArgs;
+            }
+
+            cfg.Input = free?.Skip(2).FirstOrDefault();
+            if (cfg.Input != null && !File.Exists(cfg.Input.RelativeToBaseDirectory()))
+            {
+                Console.Error.WriteLine("Input file with URLs not found");
+                Console.Error.WriteLine();
+                cfg.Error = Error.InvalidArgs;
+            }
+
+            if (cfg.ShowHelpAndExit || cfg.Error != 0 || cfg.Settings == null || free == null || free.Count < 2)
+            {
+                Console.WriteLine("Usage: webdiff [OPTIONS] URL1 URL2 [FILE]");
+                Console.WriteLine("Options:");
+                options.WriteOptionDescriptions(Console.Out);
+                Console.WriteLine();
+                Console.WriteLine("Examples:");
+                Console.WriteLine("  webdiff http://prod.example.com http://test.example.com < URLs.txt");
+                Console.WriteLine(
+                    "  webdiff -p profile.toml -t template.html -o data http://prod.example.com http://test.example.com URLs.txt");
+                Console.WriteLine();
+                return cfg;
+            }
+
+            return cfg;
+        }
+
+        //TODO: Errors handling and refactoring
+		private static bool Cmp(Configuration cfg, (Uri BaseUri, RemoteWebDriver Driver)[] items, string relative, int idx, Results results)
+        {
+            var settings = cfg.Settings;
 			var isScript = StringUtils.RemovePrefix(ref relative, "EXEC ");
 
 			var pages = items.AsParallel().AsOrdered().Select(item =>
@@ -275,7 +303,7 @@ namespace webdiff
 				Match = match,
 				Left = new Page
 				{
-					Url = isScript ? null : new Uri(SvcLeft, relative),
+					Url = isScript ? null : new Uri(cfg.SvcLeft, relative),
 					Response = pageLeft.Http,
 					Img = new Img
 					{
@@ -286,7 +314,7 @@ namespace webdiff
 				},
 				Right = new Page
 				{
-					Url = isScript ? null : new Uri(SvcRight, relative),
+					Url = isScript ? null : new Uri(cfg.SvcRight, relative),
 					Response = pageRight.Http,
 					Img = new Img
 					{
@@ -334,10 +362,7 @@ namespace webdiff
 		private const string JsRenderFunctionName = "render";
 		private const string JsResultsFilename = "results.js";
 
-		private static Uri SvcLeft;
-		private static Uri SvcRight;
-
-		private static string ResultsPath;
+        private static string ResultsPath;
 		private static string ResultsImgPath;
 	}
 }
